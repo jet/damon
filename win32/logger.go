55d2bcb1a7858aadeaeb3af339bc23067d71ad22
Justen Walker <justen.walker+github@gmail.com>
2018-09-20 13:05:24 -0400

    Add win32 log interface

---
+++ win32/logger.go
@@ -0,0 +1,66 @@
+package win32
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+// Logger interface for allowing the internal window api calls to make
+// log entries in any external logging system
+//
+// Implementers should implement this simple interface and call SetLogger to enable
+// error logging of the win32 package
+type Logger interface {
+	Error(err error, msg string)
+	Logln(v ...interface{})
+	Logf(format string, args ...interface{})
+}
+
+var globalLoggerLock sync.Mutex
+var globalLogger atomic.Value
+
+// SetLogger sets the logger to be used by the win32 api package
+// for errors and warnings
+func SetLogger(l Logger) {
+	globalLoggerLock.Lock()
+	defer globalLoggerLock.Unlock()
+	globalLogger.Store(l)
+}
+
+func init() {
+	SetLogger(noopLogger{})
+}
+
+func logger() Logger {
+	v := globalLogger.Load()
+	return v.(Logger)
+}
+
+func Logf(format string, v ...interface{}) {
+	logger().Logf(format, v...)
+}
+
+func Logln(v ...interface{}) {
+	logger().Logln(v...)
+}
+
+func LogError(err error, msg string) {
+	if err != nil {
+		logger().Error(err, msg)
+	}
+}
+
+// noopLogger silently discards logs
+type noopLogger struct{}
+
+func (n noopLogger) Logf(format string, v ...interface{}) {
+
+}
+
+func (n noopLogger) Logln(v ...interface{}) {
+
+}
+
+func (n noopLogger) Error(err error, msg string) {
+
+}
